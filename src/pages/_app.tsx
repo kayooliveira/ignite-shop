1d02206b77ebf6a9a1b5c17fd6b9182fdd664009
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-03-14 13:35:27 -0300

    revert(Removing normalize.css dependency and adding simple css reset in globals.ts): :rewind:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,3 @@
-// import '@/styles/globals.css'
-import 'normalize.css/normalize.css'
 import { globalStyles } from '@/styles/globals'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -5,5 +5,10 @@ export const globalStyles = globalCss({
     maxWidth: '100vw',
     height: '100%',
     fontFamily: "'Roboto', Arial, Helvetica, sans-serif"
+  },
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box'
   }
 })
