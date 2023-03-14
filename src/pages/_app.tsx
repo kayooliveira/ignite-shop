52e994c0a83ea97810a468e5253f2a077119d491
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-03-13 22:49:40 -0300

    feat(Adding new globalStyles from stitches and importing Roboto font): :sparkles:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
-import "@/styles/globals.css";
-import "normalize.css/normalize.css";
-import type { AppProps } from "next/app";
+// import '@/styles/globals.css'
+import 'normalize.css/normalize.css'
+import { globalStyles } from '@/styles/globals'
+import type { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  preload: true
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  globalStyles()
+  return <Component className={roboto.className} {...pageProps} />
 }
