4b9c7a98c15c2c26f7cdf3025110704c7ddcf715
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-03-13 22:49:22 -0300

    refactor(Now using stitches to define global styles): :sparkles:

---
+++ src/styles/globals.ts
@@ -0,0 +1,9 @@
+import { globalCss } from '.'
+
+export const globalStyles = globalCss({
+  'html, body, #__next': {
+    maxWidth: '100vw',
+    height: '100%',
+    fontFamily: "'Roboto', Arial, Helvetica, sans-serif"
+  }
+})
