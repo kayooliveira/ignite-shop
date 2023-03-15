1e9f436fad49ae09db3508c67ce9d73ee08eee8e
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-03-15 03:14:53 -0300

    feat(Adding DefaultLayout around app and moving globalStyles function outside the App component to prevent unecessary re-renders): :sparkles:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
+import { DefaultLayout } from '@/layout/Default'
 import { globalStyles } from '@/styles/globals'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
+
 const roboto = Roboto({
   weight: ['400', '700'],
   subsets: ['latin'],
   preload: true
 })
 
+globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
-  globalStyles()
-  return <Component className={roboto.className} {...pageProps} />
+  return (
+    <DefaultLayout>
+      <Component className={roboto.className} {...pageProps} />
+    </DefaultLayout>
+  )
 }
